1f9b42b8938bb53b75ee9e28f58c74056acc465f
Wellmai <105400940+Wellmai@users.noreply.github.com>
2024-07-22 16:30:25 -0300

    Add files via upload

---
+++ CONVERSÃO DE TEMPERATURA DE K PARA C.go
@@ -0,0 +1,15 @@
+// declarar meu pacote principal
+package main
+
+import "fmt"
+
+const ebulicaoK = 373.0
+
+func main() {
+
+	tempK := ebulicaoK
+	tempC := tempK - 273.0
+
+	fmt.Printf("A temperatura de ebulição da água em K = %g , temperatura de ebulição da água em °C =%g.", tempK, tempC)
+
+}
